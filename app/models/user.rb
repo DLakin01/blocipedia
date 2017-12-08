db7ebee9485fc8ecd70c8e290d3728a147e81cd7
DLakin01 <DLakin01@gmail.com>
2017-12-08 07:30:41 -0500

    Sign in working, params permitted

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,22 @@ class User < ApplicationRecord
 
   after_create :welcome_email
 
+  # Validations for name
+  validates :name, presence: true, length: { minimum: 1, maximum: 100 }
+
+  # Validations for password
+  validates :password, presence: true, length: { minimum: 8 }
+
+  # Validations for email
+  validates :email,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 6, maximum: 254 }
+
+def avatar_url(size)
+  gravatar_id = Digest::MD5::hexdigest(self.email).downcase
+  "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?s=#{size}"
+end
 
   private
   def welcome_email
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :user do
-    
+    name RandomData.random_name
+    email RandomData.random_email
+    password RandomData.random_sentence
   end
 end
