b003b66fcb6d3afa25bcc4d16a5f3924bf587b15
DLakin01 <DLakin01@gmail.com>
2017-12-20 16:40:38 -0500

    more migration jiggering

--- db/migrate/20171206165452_devise_create_users.rb
+++ db/migrate/20171206165452_devise_create_users.rb
@@ -2,9 +2,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
   def change(arg1 = nil, arg2 = nil)
     create_table :users do |t|
       ## Database authenticatable
-      t.name   :name,               null: false
-      t.string :email,              null: false
-      t.string :encrypted_password, null: false
+      t.string :name
+      t.string :email
+      t.string :encrypted_password
 
       ## Recoverable
       t.string   :reset_password_token
@@ -14,7 +14,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
       t.datetime :remember_created_at
 
       ## Trackable
-      t.integer  :sign_in_count, default: 0, null: false
+      t.integer  :sign_in_count, default: 0
       t.datetime :current_sign_in_at
       t.datetime :last_sign_in_at
       t.string   :current_sign_in_ip
@@ -32,7 +32,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
       # t.datetime :locked_at
 
 
-      t.timestamps null: false
+      t.timestamps
     end
 
     add_index :users, :email,                unique: true
