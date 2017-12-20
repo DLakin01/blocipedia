e75d08ffae5c9991d831be8b21dfc6fc5c86793a
DLakin01 <DLakin01@gmail.com>
2017-12-20 16:35:28 -0500

    jiggering with migration

--- db/migrate/20171206165452_devise_create_users.rb
+++ db/migrate/20171206165452_devise_create_users.rb
@@ -2,9 +2,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
   def change(arg1 = nil, arg2 = nil)
     create_table :users do |t|
       ## Database authenticatable
-      t.name   :name,               null: false, #default: ""
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.name   :name,               null: false
+      t.string :email,              null: false
+      t.string :encrypted_password, null: false
 
       ## Recoverable
       t.string   :reset_password_token
